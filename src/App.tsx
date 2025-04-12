61c77b0de48b63259ca29b8da7302eec6ea55213
sem0ark <arcadii.sem@gmail.com>
2025-04-12 13:39:14 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { HashRouter } from "react-router";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
@@ -43,4 +45,10 @@ function App() {
   );
 }
 
-export default App;
+export function Main() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  )
+}
