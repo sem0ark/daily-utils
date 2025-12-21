93096acd0b8236915da9aaeb8db12d469d533c90
sem0ark <arcadii.sem@gmail.com>
2025-12-21 14:39:26 +0100

    feat: improve component design

--- src/pages/text-processing/FormatMarkdown.tsx
+++ src/pages/text-processing/FormatMarkdown.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useState } from "react";
 import { CopyTextEntryDirect } from "../../components/copyTextEntry";
 import {
   joinFunctions,
@@ -13,33 +12,13 @@ const onPaste = joinFunctions(replaceUnicode, removeEmoji, trimLines);
 const onCopy = (text: string) => replaceMarkdownElements(text);
 
 export function FormatMarkdown() {
-  const [count, setCount] = useState(1);
-  const increase = useCallback(() => setCount((c) => c + 1), [setCount]);
-
   return (
-    <>
-      <h1 className="mb-8 w-full text-center text-3xl font-bold">
-        Format Markdown
-      </h1>
-
-      <h2 className="my-8 w-full text-center text-2xl font-bold">
-        Text Entries
-      </h2>
+    <div className="mx-auto max-w-4xl p-6">
+      <h1 className="mb-8 text-center text-3xl font-bold">Format Markdown</h1>
 
       <div className="my-5 flex flex-col gap-4">
-        {Array.from(Array(count)).map((_, index) => (
-          <CopyTextEntryDirect key={index} onPaste={onPaste} onCopy={onCopy} />
-        ))}
+        <CopyTextEntryDirect onPaste={onPaste} onCopy={onCopy} />
       </div>
-
-      <div className="flex gap-1 p-1">
-        <button
-          onClick={increase}
-          className="flex w-full items-center justify-center gap-2 rounded-lg border-2 border-neutral-500 bg-neutral-100 p-2 text-center text-xl font-bold text-blue-500 transition-all duration-400 hover:gap-4 hover:border-neutral-100 hover:text-blue-600"
-        >
-          New Entry
-        </button>
-      </div>
-    </>
+    </div>
   );
 }
